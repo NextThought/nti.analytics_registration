f263f4baa67dbcc3d4fca4748ed9752bcfcb356e
Carlos Sanchez <papachoco@gmail.com>
2017-12-11 12:23:13 -0600

    pylint;pep8

--- src/nti/analytics_registration/generations/tests/test_install.py
+++ src/nti/analytics_registration/generations/tests/test_install.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
-# disable: accessing protected members, too many methods
-# pylint: disable=W0212,R0904
+# pylint: disable=protected-access,too-many-public-methods
 
 from hamcrest import has_key
 from hamcrest import assert_that
@@ -15,6 +15,7 @@ from nti.app.testing.decorators import WithSharedApplicationMockDS
 
 from nti.dataserver.tests.mock_dataserver import mock_db_trans
 
+
 class TestFunctionalInstall(ApplicationLayerTest):
 
     @WithSharedApplicationMockDS
@@ -23,4 +24,5 @@ class TestFunctionalInstall(ApplicationLayerTest):
 
             root = conn.root()
             generations = root['zope.generations']
-            assert_that( generations, has_key('nti.dataserver-analytics_registration'))
+            assert_that(generations, 
+                        has_key('nti.dataserver-analytics_registration'))
